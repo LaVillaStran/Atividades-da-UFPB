ebfeea3ffbc4e1e90e85ab8a4fef9bbf873b194e
Henrique Jales <137725058+LaVillaStran@users.noreply.github.com>
2026-03-12 18:36:13 -0300

    ...

--- Strings/Questao2.py
+++
@@ -1,9 +0,0 @@
-def inverter(frase):
-    return frase[::-1]
-
-def main():
-    entrada = input("Digite uma frase e veja seu reverso: ")
-    print(inverter(entrada))
-
-if __name__ == "__main__":
-    main()
